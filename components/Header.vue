a8c12336520f52b9d5fb566d5943b817b9a250fe
Krzysztof Stelmach <kcezarystelmach@gmail.com>
2022-10-28 11:23:20 +0200

    Mobile menu

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,14 @@
 <template>
-  <header class="header">
+  <header
+    class="app-header"
+    :class="innerWidth < 980 && mobileMenuOpen ? 'mobile-open' : ''"
+  >
     <Logo></Logo>
     <MenuLinks class="mobile-links"></MenuLinks>
     <SocialMedia class="social-media"></SocialMedia>
-    <div class="mobile-icon">
-      <i class="fas fa-bars"></i>
-    </div>
+    <a class="mobile-icon" @click="mobileMenuOpen = !mobileMenuOpen">
+      <i :class="!mobileMenuOpen ? 'fas fa-bars' : 'fas fa-close'"></i>
+    </a>
   </header>
 </template>
 
@@ -15,10 +18,37 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "Header",
+  data() {
+    return {
+      innerWidth: 0,
+      mobileMenuOpen: false,
+    };
+  },
+
+  computed: {
+    isMobile() {
+      return process.client ? window.innerWidth < 980 : 0;
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener("resize", this.onResize);
+    });
+  },
+
+  beforeDestroy() {
+    window.removeEventListener("resize", this.onResize);
+  },
+
+  methods: {
+    onResize() {
+      this.innerWidth = process.client ? window.innerWidth : 0;
+    },
+  },
 });
 </script>
-<style lang="scss" scoped>
-.header {
+<style lang="scss">
+.app-header {
   display: flex;
   max-width: $fullhd;
   margin: 0 auto;
@@ -29,17 +59,45 @@ export default Vue.extend({
   padding: $spacing-06 $spacing-05;
   top: 0;
   z-index: 1;
+
+  &.mobile-open {
+    display: flex;
+    background: var(--color-primary-600);
+    position: fixed;
+    height: 100vh;
+    flex-direction: column;
+    align-items: flex-start;
+    z-index: 100;
+    .mobile-links {
+      flex-direction: column;
+      align-items: flex-start;
+      gap: $spacing-05;
+      a {
+        margin-left: 0;
+        font-size: 25px;
+      }
+    }
+    .mobile-icon {
+      position: absolute;
+      right: 42px;
+      top: 40px;
+    }
+  }
+
   .mobile-icon {
     display: none;
   }
   @media (max-width: #{$desktop + 1}) {
     padding: $spacing-07 $spacing-05;
-    .mobile-links,
-    .social-media {
-      display: none;
+    &:not(.mobile-open) {
+      .mobile-links,
+      .social-media {
+        display: none;
+      }
     }
     .mobile-icon {
       display: inline-block;
+      margin-bottom: $spacing-03;
       i {
         font-size: var(--font-size-2);
       }
